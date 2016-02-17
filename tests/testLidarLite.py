343164565e4739a31be0d6c25af271012f7e63b7
jonkeller <kellejm2+git@gmail.com>
2016-02-17 14:38:26 -0500

    Added friendly message

--- tests/testLidarLite.py
+++ tests/testLidarLite.py
@@ -5,6 +5,9 @@ sys.path.append('..')
 from lidarLite import Lidar_Lite
 import time
 
+NUM_TESTS = 100
+SLEEP_TIME = 1
+
 lidar = Lidar_Lite()
 connected = lidar.connect(1)
 
@@ -13,9 +16,11 @@ if connected <= -1:
     exit(1)
 
 print "Connected"
-for i in range(100):
+print "Going to test", NUM_TESTS, "times: once every", SLEEP_TIME, "second(s)."
+
+for i in range(NUM_TESTS):
     try:
-        time.sleep(1)
+        time.sleep(SLEEP_TIME)
         distance = lidar.getDistance()
         print "Distance:", distance, "cm"
     except:
