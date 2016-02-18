ae1b8c8fb47f9d75c363c23e47de78c123000e49
jonkeller <kellejm2+git@gmail.com>
2016-02-17 21:22:56 -0500

    Fixed errors

--- tests/3DLidarScan.py
+++ tests/3DLidarScan.py
@@ -16,15 +16,14 @@ TILT_MIN_ANGLE = -45
 TILT_MAX_ANGLE = 90
 PAN_MIN_ANGLE = -90
 PAN_MAX_ANGLE = 90
+SLEEP_TIME = 1
 
 def pan(angle):
     us = angleToMicroseconds(angle)
-    print angle, us, RIGHT, LEFT
     servo.set_servo(PAN_SERVO_PIN, us)
 
 def tilt(angle):
     us = angleToMicroseconds(angle)
-    print angle, us, RIGHT, LEFT
     servo.set_servo(TILT_SERVO_PIN, us)
 
 def angleToMicroseconds(angle):
@@ -36,6 +35,7 @@ def cleanup():
     servo.stop_servo(PAN_SERVO_PIN)
     servo.stop_servo(TILT_SERVO_PIN)
 
+PWM.set_loglevel(PWM.LOG_LEVEL_ERRORS)
 servo = PWM.Servo()
 lidar = Lidar_Lite()
 connected = lidar.connect(1)
@@ -43,18 +43,19 @@ connected = lidar.connect(1)
 if connected <= -1:
     print "Not Connected"
     exit(1)
-print "Connected"
 
-print "Pan_Angle, Tilt_Angle, Distance (cm)"
+print "Pan_Angle, Tilt_Angle, Distance (cm), Error"
 INCREMENT = 2
 for t in range(TILT_MIN_ANGLE, TILT_MAX_ANGLE+1, INCREMENT):
+    tilt(t)
     for p in range(PAN_MIN_ANGLE, PAN_MAX_ANGLE+1, INCREMENT):
+        pan(p)
         try:
             time.sleep(SLEEP_TIME)
             distance = lidar.getDistance()
-            print p, ",", t, ",", distance
+            print p, ",", t, ",", distance, ","
         except:
-            print p, ",", t, ",", -1
+            print p, ",", t, ",", -1, sys.exc_info()[0]
             pass
 
 cleanup()
